a58c93d28889e83d235ec7a109eba77f84b0d992
Ayumi <ayumihosoda@gmail.com>
2017-05-15 22:50:44 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 function commentApp() {
 	// pull input data and store in variable
-	var name = document.getElementById("name").value
-	var comment = document.getElementById("comment").value
+	var nameInput = document.getElementById("name").value
+	var commentInput = document.getElementById("comment").value
 	// make the above var into text node and name them as var
-	var nameText = document.createTextNode("Name: " + name)
-	var commentText = document.createTextNode("Comment: " + comment)
-	// create element h3, p
-	var newName = document.createElement("h3")
-	var newComment = document.createElement("p")
-	// add nameText and commentText inside newly created elements
-	newName.appendChild(nameText)
-	newComment.appendChild(commentText)
-	// create element li and add this u
+	var nameText = document.createTextNode("My Name: " + nameInput)
+	var commentText = document.createTextNode("Message : " + commentInput)
+	//create li
 	var newList = document.createElement("li")
+	// //create class for li
 	newList.className = "list-group-item"
-	// add newName and newComment under newList
+	// create h3 and p tags for nameText and comment Text
+	var newName = document.createElement("h3")
+	newName.appendChild(nameText)
+	// create p tag
+	var newComment = document.createElement("p")
+	newComment.appendChild(commentText)
+	// add var nameText and commentText under li
 	newList.appendChild(newName)
 	newList.appendChild(newComment)
+	// append li to ul
 	document.getElementById("list").appendChild(newList)
-
 }
